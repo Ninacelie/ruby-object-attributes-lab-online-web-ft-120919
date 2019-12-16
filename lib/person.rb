37cf0a16e537f46622a3039680f480200bdea938
Nina Angelo <ninacelie@gmail.com>
2019-12-16 21:16:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,15 @@
 class Person
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+end
+
+
+class Person
   def job=(persons_job)
     @job = persons_job
 end
